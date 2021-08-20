5c2266d26f3281d7e0b0d8ec718caae833fc80da
bsolism <bsolism@outlook.com>
2021-08-19 21:27:21 -0600

    refactory_profile2

---
+++ app/models/RefugeHouse.js
@@ -0,0 +1,31 @@
+const RefugeHouse = [
+  {
+    id: 1,
+    name: "House Pet",
+    city: "SPS",
+    address: "2 ave. 4 calle",
+    phone: "93949495",
+    email: "house@pet.com",
+    photo: require("../assets/housePets.jpg"),
+  },
+  {
+    id: 2,
+    name: "House Cat",
+    city: "SPS",
+    address: "Bo. Cabañas",
+    phone: "9000005",
+    email: "house@pet.com",
+    photo: require("../assets/housePets2.jpg"),
+  },
+  {
+    id: 2,
+    name: "Rescue Dog",
+    city: "SPS",
+    address: "Bo. Barandilla",
+    phone: "44444444",
+    email: "house@pet.com",
+    photo: require("../assets/housePets3.jpg"),
+  },
+];
+
+export default RefugeHouse;
