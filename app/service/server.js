62ed7e7dd9f02cf5e29a891f3f25f4fae5f4206d
bsolism <bsolism@outlook.com>
2021-12-18 19:19:02 -0600

    Publish

--- app/service/server.js
+++ app/service/server.js
@@ -1,6 +1,6 @@
 export default {
-  URI: "http://192.168.50.8:45455/api",
-  //URI: "http://bsolispetme-001-site1.gtempurl.com/api",
+  //URI: "http://192.168.50.8:45455/api",
+  URI: "http://bsolispetme-001-site1.gtempurl.com/api",
   //URI: "http://bsolis02-001-site1.ftempurl.com/api",
   //URI: "https://127.0.0.1:45455/api",
   //URI: "https://petme.azurewebsites.net/api",
